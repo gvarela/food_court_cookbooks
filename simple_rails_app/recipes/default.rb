5085d70f37b5b9556f74193316a88e64a6317eea
Gabe Varela <gvarela@gmail.com>
2010-09-18 23:59:53 -0600

    fix up the simple_rails_app recipes based on first run

--- simple_rails_app/recipes/default.rb
+++ simple_rails_app/recipes/default.rb
@@ -4,44 +4,46 @@ node[:apps].each do |app|
   home_path = "/home/#{app[:username]}"
   repos_path = "#{home_path}/repos/#{app[:name]}.git"
   app_path = "#{home_path}/#{app[:name]}"
-  
+
   directory "#{repos_path}" do
     recursive true
     owner app[:username]
-    group app[:group] || app[:username]    
+    group app[:group] || app[:username]
   end
 
   # initialize bare git repo
   bash "create repo folder" do
     user app[:username]
-    group app[:group] || app[:username]    
+    group app[:group] || app[:username]
     cwd repos_path
     code "git init --bare"
     not_if { File.exists?("#{repos_path}/.git") }
   end
-  
+
   template "#{repos_path}/hooks/post-receive" do
     path "#{repos_path}/hooks/post-receive"
     source "post-receive.erb"
     owner app[:username]
-    group app[:group] || app[:username]    
+    group app[:group] || app[:username]
     mode 0755
     variables(
       :app_path => app_path,
-      :git_branch => app[:git_branch] || "master"
+      :git_branch => app[:git_branch] || "master",
+      :pre_migration => app[:pre_migration],
+      :post_migration => app[:post_migration]
     )
     action :create
   end
-    
+
   # set web app permissions
   bash "clone git repo" do
     user app[:username]
-    group app[:group] || app[:username]    
+    group app[:group] || app[:username]
     cwd home_path
     code "git clone #{repos_path} #{app[:name]}"
     not_if { File.exists?(app_path) }
   end
-  
+
   template "#{node[:nginx][:conf_dir]}/sites-available/#{app[:name]}" do
     source "rails_app.conf.erb"
     owner "root"
@@ -52,7 +54,7 @@ node[:apps].each do |app|
       :server_name => app[:server]
     )
     if File.exists?("#{node[:nginx][:conf_dir]}/sites-enabled/#{app[:name]}")
-      notifies :reload, resources(:service => "nginx"), :delayed
+      notifies :restart, resources(:service => "nginx"), :delayed
     end
     not_if { File.exists?("#{node[:nginx][:conf_dir]}/sites-enabled/#{app[:name]}") }
   end
